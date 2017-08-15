65641c22fa244cc00ba98d9d24b5766b949daf64
Meryl Turner <merylturner08@gmail.com>
2017-08-15 11:49:58 -0700

    added border options

--- react-editor/src/App.js
+++ react-editor/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
 
 const largeHeader = <h1>Welcome to React Editor</h1>;
@@ -16,7 +15,9 @@ class App extends Component {
             selectedFont: 'option1',
             selectedMargin: 'option1',
             userName: props.userName,
-            selectedFontSize: 'option1'
+            selectedFontSize: 'option1',
+            selectedFontStyle: 'option1',
+            selectedBorder: 'option1'
         };
     }
 
@@ -26,11 +27,6 @@ class App extends Component {
         });
     }
 
-    // handleOptionChange(event) {
-    //     this.setState({
-    //         selectedOption: event.target.value
-    //     });
-    // }
 
     render() {
 
@@ -39,39 +35,40 @@ class App extends Component {
                 {largeHeader}
                 <div>
                     <div>
-            Hey {this.state.userName}
+            Hey there, {this.state.userName}
                     </div>
 
                     <div>
+                        <h3>What's your name?</h3>
                         <label>
-                            <h3>UserName</h3>
                             <input name="userName" value={this.state.userName} onChange={({ target }) => this.handleChange(target)} />
                         </label>
+                        Let's play with some text below.
+                        <h3>Message</h3>
                         <label>
-                            <h3>Message</h3>
                             <input name="message" value={this.state.message} onChange={({ target }) => this.handleChange(target)} />
                         </label>
+                        <h3>Color</h3>
                         <label>
-                            <h3>Color</h3>
                             <input name="color" type="color" value={this.state.color} onChange={({ target }) => this.handleChange(target)} />
                         </label>
                     </div>
 
                     <div className="radioButtons">
+                        <h3>Margin</h3>
                         <label>
-                            <h3>Margin</h3>
                             <input type="radio" name="selectedMargin" value="300px" checked={this.state.selectedMargin === '300px'} onChange={({ target }) => this.handleChange(target)} />
                         Large
                         </label>
                         <label>
-                            <input type="radio" name="selectedMargin" value="100px" checked={this.state.selectedMargin === '100px'} onChange={({ target }) => this.handleChange(target)} />
+                            <input type="radio" name="selectedMargin" value="50px" checked={this.state.selectedMargin === '50px'} onChange={({ target }) => this.handleChange(target)} />
                         Small
                         </label>
                     </div>
 
                     <div className="radioButtons">
+                        <h3>Font Family </h3>
                         <label>
-                            <h3>Font Family </h3>
                             <input type="radio" name="selectedFont" value="Arial" checked={this.state.selectedFont === 'Arial'} onChange={({ target }) => this.handleChange(target)} />
                         Sans Serif
                         </label>
@@ -82,8 +79,8 @@ class App extends Component {
                     </div>
                     
                     <div className="radioButtons">
+                        <h3>Font Size </h3>
                         <label>
-                            <h3>Font Size </h3>
                             <input type="radio" name="selectedFontSize" value="72px" checked={this.state.selectedFont === '72px'} onChange={({ target }) => this.handleChange(target)} />
                         Large
                         </label>
@@ -92,9 +89,43 @@ class App extends Component {
                         Small
                         </label>
                     </div>
+                    
+                    <div className="radioButtons">
+                        <h3>Font Style</h3>
+                        <label>
+                            <input type="radio" name="selectedFontStyle" value="italic" checked={this.state.selectedFontStyle === 'italic'} onChange={({ target }) => this.handleChange(target)} />
+                            Italic
+                        </label>
+                        <label>
+                            <input type="radio" name="selectedFontStyle" value="normal" checked={this.state.selectedFontStyle === 'normal'} onChange={({ target }) => this.handleChange(target)} />
+                            None
+                        </label>
+                    </div>
+
+                    <div className="radioButtons">
+                        <h3>Border</h3>
+                        <label>
+                            <input type="radio" name="selectedBorder" value="1px solid black" checked={this.state.selectedBorder === '1px solid black'} onChange={({ target }) => this.handleChange(target)} />
+                            Thin
+                        </label>
+                        <label>
+                            <input type="radio" name="selectedBorder" value="5px dashed blue" checked={this.state.selectedBorder === '5px dashed blue'} onChange={({ target }) => this.handleChange(target)} />
+                            Thick
+                        </label>
+                        <label>
+                            <input type="radio" name="selectedBorder" value="none" checked={this.state.selectedBorder === 'none'} onChange={({ target }) => this.handleChange(target)} />
+                            None
+                        </label>
+                    </div>
 
                 </div>
-                <div className="display" style={{ color: this.state.color, fontFamily: this.state.selectedFont, margin: this.state.selectedMargin, fontSize: this.state.selectedFontSize }}>
+                <div className="display" style={{ 
+                    color: this.state.color, 
+                    fontFamily: this.state.selectedFont, 
+                    margin: this.state.selectedMargin, 
+                    fontSize: this.state.selectedFontSize, 
+                    fontStyle:this.state.selectedFontStyle,
+                    border: this.state.selectedBorder }}>
                     {this.state.message}
                 </div>
             </div>
