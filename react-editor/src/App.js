e65159286d25ee5ed920d26d0813621b2eb2672e
Meryl Turner <merylturner08@gmail.com>
2017-08-15 10:58:18 -0700

    added radio buttons

--- react-editor/src/App.js
+++ react-editor/src/App.js
@@ -3,35 +3,69 @@ import React, { Component } from 'react';
 import './App.css';
 
 const largeHeader = <h1>Welcome to React Editor</h1>;
-const smallHeader = <h3>React is pretty sweet</h3>;
 
 class App extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            name: 'Brah',
-            message: props.initialMessage
+            name: 'Lewis the Pup',
+            message: props.initialMessage,
+            color: '#7109AA',
+            margin: '100px',
+            selectedOption: 'option1'
         };
     }
 
-    handleChange(value) {
+    handleChange({ name, value }) {
         this.setState({
-            name: value,
+            [name]: value,
+        });
+    }
+
+    handleOptionChange(event) {
+        this.setState({
+            selectedOption: event.target.value
         });
     }
 
     render() {
 
-        const header = this.state.message.length > 15 ? largeHeader : smallHeader;
         return (
-            <div>
-              {header}
-                <div className="App">
-          Hey {this.state.name}
-                </div>
+            <div className="App">
+                {largeHeader}
                 <div>
-                    <input value={this.state.name} onChange={({target}) => this.handleChange(target.value)}/>
+                    <div>
+            Hey {this.state.name}
+                    </div>
+                    <div>
+                        <label>
+                            <h3>Message </h3>
+                            <input name="message" value={this.state.message} onChange={({ target }) => this.handleChange(target)} />
+                        </label>
+                        <label>
+                            <h3>Color</h3>
+                            <input name="color" type="color" value={this.state.color} onChange={({ target }) => this.handleChange(target)} />
+                        </label>
+                        <label>
+                            <h3>Margin</h3>
+                            <input name="margin" value={this.state.margin} onChange={({ target }) => this.handleChange(target)} />
+                        </label>
+                    </div>
+                    <div className="radioButtons">
+                        <label>
+                            <h3>Font Family </h3>
+                            <input type="radio" name="selectedOption" value="Arial" checked={this.state.selectedOption === 'Arial'} onChange={({ target }) => this.handleChange(target)} />
+                        Sans Serif
+                        </label>
+                        <label>
+                            <input type="radio" name="selectedOption" value="Times" checked={this.state.selectedOption === 'Times'} onChange={({ target }) => this.handleChange(target)} />
+                        Serif
+                        </label>
+                    </div>
+                </div>
+                <div className="display" style={{ color: this.state.color, fontFamily: this.state.selectedOption }}>
+                    {this.state.message}
                 </div>
             </div>
         );
